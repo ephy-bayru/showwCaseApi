336caba5e88a589347bd08538969129173be4ff6
ephy-bayru <ephybayru@gmail.com>
2023-07-25 22:45:26 +0300

    feat(app): Introduce AppBuilder for modular app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,70 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import routes from "./routes/index";
+import logger from "./utils/logger.util";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import swaggerOptions from "../swaggerConfig";
+import ResponseHandler from "./helpers/httpResponse.helper";
+import { rateLimiter } from "./middlewares/rateLimit.middleware";
+import errorMiddleware from "./middlewares/error.middleware";
+
+class AppBuilder {
+  private app: Express;
+
+  constructor() {
+    this.app = express();
+  }
+
+  setupMiddlewares(): AppBuilder {
+    this.app.use(rateLimiter);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cors());
+    this.app.use(helmet());
+
+    // Middleware for logging requests
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      logger.info(`${req.method} ${req.url}`);
+      next();
+    });
+
+    // Error middleware
+    this.app.use(errorMiddleware);
+
+    return this;
+  }
+
+  setupSwagger(): AppBuilder {
+    const swaggerSpec = swaggerJsdoc(swaggerOptions);
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    return this;
+  }
+
+  setupRoutes(): AppBuilder {
+    this.app.use("/api", routes);
+    this.app.get("/", (req, res) => {
+      res.redirect("/api/health");
+    });
+
+    // Universal route
+    this.app.use((_req: Request, res: Response) => {
+      ResponseHandler.send(res, 404, null, "404 Not Found");
+    });
+
+    return this;
+  }
+
+  build(): Express {
+    return this.app;
+  }
+}
 
 export function createAppInstance(): Express {
-  const app = express();
-
-  setupMiddlewares(app);
-  setupSwagger(app);
-
-  return app;
-}
-
-function setupMiddlewares(app: Express) {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cors());
-  app.use(helmet());
-}
-
-function setupSwagger(app: Express) {
-  const swaggerSpec = swaggerJsdoc(swaggerOptions);
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  return new AppBuilder()
+    .setupMiddlewares()
+    .setupSwagger()
+    .setupRoutes()
+    .build();
 }
