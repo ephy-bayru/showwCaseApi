bd662f4b9ac1e894f91b1b9f8bfacdad70128873
ephy-bayru <ephybayru@gmail.com>
2023-08-03 06:38:20 +0300

    feat(repository): Implement BaseRepository class and IBaseRepository interface
    - Define a base repository interface (IBaseRepository) with common CRUD operations and additional utility methods
    - Create a generic BaseRepository class that implements IBaseRepository
      - Includes methods for creating, reading, updating, deleting, and searching documents
      - Provides a bulk creation method for inserting multiple documents at once
      - Incorporates optional methods for counting documents and checking existence
    - Utilize Mongoose Model and FilterQuery for type safety and query flexibility

--- src/repositories/interfaces/IBaseRepository.ts
+++ src/repositories/interfaces/IBaseRepository.ts
@@ -1,17 +1,22 @@
 import { FilterQuery, Document, QueryOptions } from 'mongoose';
 
+export interface RepositoryOptions {
+  [key: string]: any;
+}
+
 interface IBaseRepository<T extends Document> {
   create(entity: Partial<T>): Promise<T>;
-  updateById(id: string, entity: Partial<T>): Promise<T | null>;
-  getById(id: string): Promise<T | null>;
-  deleteById(id: string): Promise<T | null>;
-  getAll(): Promise<T[]>;
-  search(query: FilterQuery<T>, options?: QueryOptions): Promise<T[]>;
-  
+  bulkCreate(entities: Partial<T>[]): Promise<T[]>;
+  updateById(params: { id: string; entity: Partial<T>; options?: RepositoryOptions }): Promise<T | null>;
+  getById(params: { id: string; options?: RepositoryOptions }): Promise<T | null>;
+  deleteById(params: { id: string; options?: RepositoryOptions }): Promise<T | null>;
+  getAll(options?: RepositoryOptions): Promise<T[]>;
+  search(query: FilterQuery<T>, options?: QueryOptions & RepositoryOptions): Promise<T[]>;
+
   // Optionals
   count(query: FilterQuery<T>): Promise<number>;
   exists(query: FilterQuery<T>): Promise<boolean>;
-  findOne(query: FilterQuery<T>, options?: QueryOptions): Promise<T | null>;
+  findOne(query: FilterQuery<T>, options?: QueryOptions & RepositoryOptions): Promise<T | null>;
 }
 
 export default IBaseRepository;
