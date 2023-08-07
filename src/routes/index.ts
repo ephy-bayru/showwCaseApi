b75dc62ff0c1870b1c60582246813be9b88da555
ephy-bayru <ephybayru@gmail.com>
2023-08-07 07:32:05 +0300

    auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import healthRoutes from '../components/health/v1/health.routes';
 import usersRoutes from '../components/users/web/users.routes';
 import educationExperienceRoutes from '../components/education_experience/web/education_experience.routes';
+import authRoutes from '../components/auth/web/auth.routes';
 
 interface Route {
   path: string;
@@ -11,7 +12,8 @@ interface Route {
 const routes: Route[] = [
   { path: '/v1/health', handler: healthRoutes },
   { path: '/v1/users', handler: usersRoutes },
-  { path: '/v1/education-experiences', handler: educationExperienceRoutes }
+  { path: '/v1/education-experiences', handler: educationExperienceRoutes },
+  { path: '/v1/auth', handler: authRoutes }
 ];
 
 const createRouter = (routes: Route[]): Router => {
