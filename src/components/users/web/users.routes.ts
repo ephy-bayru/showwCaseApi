f52bc43ed940162070d6beea0ba5259957aa56e4
ephy-bayru <ephybayru@gmail.com>
2023-08-07 07:31:12 +0300

    feat: add authentication and authorization to users endpoints

--- src/components/users/web/users.routes.ts
+++ src/components/users/web/users.routes.ts
@@ -7,21 +7,64 @@ import asyncHandler from "../../../utils/asyncHandler";
 import EducationExperienceService from "../../education_experience/services/educationExperience.service";
 import EducationExperienceModel from "../../education_experience/data/schemas/EducationExperienceModel";
 import EducationExperienceRepository from "../../education_experience/data/repository/educationExperienceRepository";
+import { Permission } from "../../../config/permissions";
+import { authenticate } from "../../auth/middlewares/authenticate";
+import { authorize } from "../../auth/middlewares/authorize";
 
-const router = express.Router();
+const userRouter = express.Router();
 
 const usersRepository = new UsersRepository(UserModel);
-const educationExperienceRepository = new EducationExperienceRepository(EducationExperienceModel);
-const educationExperienceService = new EducationExperienceService(educationExperienceRepository);
+const educationExperienceRepository = new EducationExperienceRepository(
+  EducationExperienceModel
+);
+const educationExperienceService = new EducationExperienceService(
+  educationExperienceRepository
+);
 
-const usersService = new UsersService(usersRepository, educationExperienceService);
+const usersService = new UsersService(
+  usersRepository,
+  educationExperienceService
+);
 const usersController = new UsersController(usersService);
 
-router.get("/", asyncHandler(usersController.getUsers.bind(usersController)));
-router.get("/:id", asyncHandler(usersController.getUserById.bind(usersController)));
-router.post("/", asyncHandler(usersController.createUser.bind(usersController)));
-router.put("/:id", asyncHandler(usersController.updateUserById.bind(usersController)));
-router.delete("/:id", asyncHandler(usersController.deleteUserById.bind(usersController)));
-router.get("/search/:query", asyncHandler(usersController.searchUsers.bind(usersController)));
+userRouter.get(
+  "/",
+  authenticate,
+  authorize(Permission.VIEW_USERS),
+  asyncHandler(usersController.getUsers.bind(usersController))
+);
 
-export default router;
+userRouter.get(
+  "/:id",
+  authenticate,
+  authorize(Permission.VIEW_USER),
+  asyncHandler(usersController.getUserById.bind(usersController))
+);
+
+userRouter.post(
+  "/",
+  asyncHandler(usersController.createUser.bind(usersController))
+);
+
+userRouter.put(
+  "/:id",
+  authenticate,
+  authorize(Permission.UPDATE_USER),
+  asyncHandler(usersController.updateUserById.bind(usersController))
+);
+
+userRouter.delete(
+  "/:id",
+  authenticate,
+  authorize(Permission.DELETE_USER),
+  asyncHandler(usersController.deleteUserById.bind(usersController))
+);
+
+userRouter.get(
+  "/search/:query",
+  authenticate,
+  authorize(Permission.SEARCH_USERS),
+  asyncHandler(usersController.searchUsers.bind(usersController))
+);
+
+export default userRouter;
