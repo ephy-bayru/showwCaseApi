ffd86bd2d889fba866be7b979bc3eb444fdc499f
ephy-bayru <ephybayru@gmail.com>
2023-08-07 07:30:56 +0300

    feat: add authentication and authorization to education experience endpoints

--- src/components/education_experience/web/education_experience.routes.ts
+++ src/components/education_experience/web/education_experience.routes.ts
@@ -4,19 +4,87 @@ import EducationExperienceController from "./educationExperience.controllers";
 import EducationExperienceRepository from "../data/repository/educationExperienceRepository";
 import EducationExperienceModel from "../data/schemas/EducationExperienceModel";
 import asyncHandler from "../../../utils/asyncHandler";
+import { Permission } from "../../../config/permissions";
+import { authenticate } from "../../auth/middlewares/authenticate";
+import { authorize } from "../../auth/middlewares/authorize";
 
 const router = express.Router();
 
-const educationExperienceRepository = new EducationExperienceRepository(EducationExperienceModel);
-const educationExperienceService = new EducationExperienceService(educationExperienceRepository);
+const educationExperienceRepository = new EducationExperienceRepository(
+  EducationExperienceModel
+);
+const educationExperienceService = new EducationExperienceService(
+  educationExperienceRepository
+);
 
-const educationExperienceController = new EducationExperienceController(educationExperienceService);
+const educationExperienceController = new EducationExperienceController(
+  educationExperienceService
+);
 
-router.post("/:userId", asyncHandler(educationExperienceController.createEducationExperiences.bind(educationExperienceController)));
-router.get("/user/:userId", asyncHandler(educationExperienceController.getAllEducationExperiencesByUserId.bind(educationExperienceController)));
-router.get("/:id", asyncHandler(educationExperienceController.getEducationExperienceById.bind(educationExperienceController)));
-router.put("/:id", asyncHandler(educationExperienceController.updateEducationExperienceById.bind(educationExperienceController)));
-router.delete("/:id", asyncHandler(educationExperienceController.deleteEducationExperienceById.bind(educationExperienceController)));
-router.get("/autocomplete/:query", asyncHandler(educationExperienceController.autoCompleteSchoolName.bind(educationExperienceController)));
+router.post(
+  "/:userId",
+  authenticate,
+  authorize(Permission.CREATE_EDUCATION),
+  asyncHandler(
+    educationExperienceController.createEducationExperiences.bind(
+      educationExperienceController
+    )
+  )
+);
+
+router.get(
+  "/user/:userId",
+  authenticate,
+  authorize(Permission.VIEW_USER_EDUCATIONS),
+  asyncHandler(
+    educationExperienceController.getAllEducationExperiencesByUserId.bind(
+      educationExperienceController
+    )
+  )
+);
+
+router.get(
+  "/:id",
+  authenticate,
+  authorize(Permission.VIEW_EDUCATION),
+  asyncHandler(
+    educationExperienceController.getEducationExperienceById.bind(
+      educationExperienceController
+    )
+  )
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  authorize(Permission.UPDATE_EDUCATION),
+  asyncHandler(
+    educationExperienceController.updateEducationExperienceById.bind(
+      educationExperienceController
+    )
+  )
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authorize(Permission.DELETE_EDUCATION),
+  asyncHandler(
+    educationExperienceController.deleteEducationExperienceById.bind(
+      educationExperienceController
+    )
+  )
+);
+
+router.get(
+  "/autocomplete/:query",
+  authenticate,
+  authorize(Permission.AUTOCOMPLETE_SCHOOL),
+  asyncHandler(
+    educationExperienceController.autoCompleteSchoolName.bind(
+      educationExperienceController
+    )
+  )
+);
 
 export default router;
